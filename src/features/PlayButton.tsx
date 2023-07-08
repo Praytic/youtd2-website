863ae740cc1b734a44cff58163c70c066b87a5b1
Praytic <chernogorovvm@gmail.com>
2023-07-08 00:56:10 -0700

    Add warning messages for diff platforms and add favicon

--- src/features/PlayButton.tsx
+++ src/features/PlayButton.tsx
@@ -1,5 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
+
+const Warning = styled.div`
+  position: absolute;
+  padding: 20px 200px;
+  min-width: 300px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  color: #a91b1b;
+  font-size: 1.5em+1vmax;
+  margin-bottom: 1em;
+  user-select: none;
+  font-family: 'Friz Quadrata', sans-serif;
+  text-shadow: 0 1px rgba(0, 0, 0, 0.2);
+`
 
 const Button = styled.button`
   -webkit-text-size-adjust: 100%;
@@ -39,16 +53,46 @@ interface PlayButtonProps {
 
 const PlayButton: React.FC<PlayButtonProps> = ({ gameProps}) => {
   const [gameStarted, setGameStarted] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const startGame = () => {
-    setGameStarted(true);
-    // Here you would implement the logic to start the game
+    if (!disabled) {
+      setGameStarted(true);
+      // Here you would implement the logic to start the game
+    }
   };
-  // const isMacOS = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
+  // Check platform/browser conditions
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
+    const isMacOS = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+    const browser = navigator.appVersion;
+    const edgeVersion = browser.indexOf('Edge/') > -1 ? parseInt(browser.split('Edge/')[1]) : Infinity;
+    const chromeVersion = browser.indexOf('Chrome/') > -1 ? parseInt(browser.split('Chrome/')[1]) : Infinity;
+    const firefoxVersion = browser.indexOf('Firefox/') > -1 ? parseInt(browser.split('Firefox/')[1]) : Infinity;
+    const operaVersion = browser.indexOf('Opera/') > -1 ? parseInt(browser.split('Opera/')[1]) : Infinity;
+    const safariVersion = browser.indexOf('Safari/') > -1 ? parseInt(browser.split('Safari/')[1]) : Infinity;
+
+    if (isMobile) {
+      setDisabled(true);
+      setErrorMessage("Current version of the game doesn't support mobile user experience.");
+    } else if (isMacOS) {
+      setErrorMessage("Current version of the game has known issues for users with your platform. Please download the game <here> if you experience any issues during loading.");
+    } else if (edgeVersion < 79 || chromeVersion < 68 || firefoxVersion < 79 || operaVersion < 55 || safariVersion < 15.2) {
+      setDisabled(true);
+      setErrorMessage("You have unsupported browser version. Please update your browser.");
+    }
+  }, []);
+
   return (
-    // <Button onClick={startGame} disabled={isMacOS}>PLAY</Button>
-    <a href={process.env.PUBLIC_URL + "/game/index.html"} onClick={startGame}>
-      <Button disabled={gameStarted}>PLAY</Button>
-    </a>
+    <div>
+      <a href={process.env.PUBLIC_URL + "/game/index.html"} onClick={startGame}>
+        <Button disabled={gameStarted || disabled}>PLAY</Button>
+      </a>
+      <Warning>{errorMessage}</Warning>
+    </div>
   );
 };
 
