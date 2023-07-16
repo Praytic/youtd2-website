257e183d7d6b8936678d77b09596b3dfe8f730e7
Praytic <chernogorovm@gmail.com>
2023-07-15 21:48:29 -0700

    Rename workflow file

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ init({
   integrations: [
     new BrowserTracing()
   ],
+  release: "youtd2@0.4.4",
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
  });
